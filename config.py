06c139aafb30a4d83f617dd74858386aa30e3bd4
Ivan Panov <57429347+headst1337@users.noreply.github.com>
2022-10-15 07:48:52 +0700

    clean imports
    clean imports

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-import os
-
 class Config:
     DEBUG = False
     CSRF_ENABLED = True
--- server.py
+++ server.py
@@ -1,7 +1,5 @@
 import json
 import os
-import turtle
-from wsgiref.handlers import read_environ
 from flask import Flask, render_template, request
 from yandex_map_api import YandexMapApi
 
