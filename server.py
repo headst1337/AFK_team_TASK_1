c15930ec8b3c3dca3b3c9dc14aa63024664f2f1b
Ivan Panov <57429347+headst1337@users.noreply.github.com>
2022-10-16 21:43:40 +0700

    Garbage cleaning
    Garbage cleaning

--- server.py
+++ server.py
@@ -2,18 +2,18 @@ import json
 import os
 from flask import Flask, render_template, request
 
-import config
 from utils import Utils
 from yandex_map_api import YandexMapApi
 
+
 app = Flask(__name__)
-# app.config.from_object(config.DevelopmentConfig)
+app.config.from_object(os.environ['FLASK_CONFIG'])
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST' and Utils.form_validation(request.form):
-        # Получение данных с формы (ИЛЯ!!!)
+        # Получение данных с формы
         index = request.form.get('index')
         geo_coords_longitude = request.form.get('geo_coords_longitude')  # Longitude - долгота
         geo_coords_latitude = request.form.get('geo_coords_latitude')  # Latitude - широта
@@ -43,16 +43,14 @@ def index():
                                                                               "geo_range": ""})
     return render_template('index.html')
 
-
 # Принимает в себя до 3-х параметров: int index, dict geo_cords, float geo_range
 def get_data_from_api(**data) -> str:
-    api = YandexMapApi("a1910da4-923c-41a1-b952-e3de90a6859a")
+    api = YandexMapApi(os.environ['YANDEX_MAP_API'])
     if data.get("index"):
         data_from_api = api.get_by_index(data.get("index"))
     else:
         data_from_api = api.get_by_range(center=data.get("geo_coords"), spn=data.get("geo_range"))
     return data_from_api
-
 
 def parse_data_from_api(data_from_api) -> dict:
     data = json.loads(data_from_api)
@@ -69,7 +67,6 @@ def parse_data_from_api(data_from_api) -> dict:
         coords = (lo_coord, la_coord)
         data_dict.append([name, address, index, coords])
     return data_dict
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
