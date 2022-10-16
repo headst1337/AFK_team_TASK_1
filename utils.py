a969a965fe4aab7e8b09891d20841b3d2d790cf1
Demono5060 <demono5060@mail.ru>
2022-10-16 15:30:00 +0700

    Float util

--- utils.py
+++ utils.py
@@ -4,3 +4,11 @@ class Utils:
         dms = dms.split(':')
         degree, minutes, seconds = float(dms[0]), float(dms[1]), float(dms[2])
         return round(degree + minutes/60 + seconds/3600, 6)
+
+    @staticmethod
+    def isfloat(num):
+        try:
+            float(num)
+            return True
+        except ValueError:
+            return False
